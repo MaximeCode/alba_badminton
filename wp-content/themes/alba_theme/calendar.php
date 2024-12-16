20406996b16b5c34222234fd15ff67be1c3ad533
Maxime Baude <contact.mbaude@gmail.com>
2024-12-16 13:51:17 +0100

    Calendar already finished !

--- wp-content/themes/alba_theme/calendar.php
+++ wp-content/themes/alba_theme/calendar.php
@@ -7,6 +7,13 @@ get_header();
 
     <section>
         <?= display_titlePage() ?>
+
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSXDxaqPt2o4XB6CJeqO7ZI_1o_QOMVM6zt_oHiH0dgSYqyhvSXokMUTo_CQNLgdCP9GdAomkdNCiv_/pubhtml?gid=1556273338&amp;single=true&amp;widget=true&amp;headers=false"
+                class="w-full h-[90vh]">
+        </iframe>
+        <p class="text-right text-lg mt-6">Source : CODEP (site officiel :
+            <a class="underline text-primary-blue" href="https://badminton28.fr/" title="Site officiel du CODEP">badminton.fr</a>)
+        </p>
     </section>
 
 <?php
