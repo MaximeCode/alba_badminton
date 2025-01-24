86c03b1ac03ce15bbc57531d2e5f22608f2437d1
Maxime Baude <contact.mbaude@gmail.com>
2025-01-24 15:57:17 +0100

    filter by categories added in all actu

--- wp-content/themes/alba_theme/actualites.php
+++ wp-content/themes/alba_theme/actualites.php
@@ -5,10 +5,30 @@ global $alba_theme_variables;
 get_header();
 
 $category = $_GET['categoryName'] ?? null;
+var_dump($category);
+//echo '<pre>';
+//var_dump(get_categories());
+//echo '</pre>';
+// get_categories()[0]['name']
+$actif = ' bg-primary-blue text-white ';
 ?>
 
     <section>
         <?= display_titlePage() ?>
+
+        <h2 class="text-2xl underline decoration-primary-blue">Filtrer les actualités par catégories :</h2>
+
+        <div class="my-8 font-semibold">
+            <?php foreach (get_categories() as $cat) : ?>
+                <a href="<?= get_permalink(26) ?>?categoryName=<?= $cat->slug ?>"
+                   class="<?php echo $alba_theme_variables['animBase'];
+                   echo $category == $cat->slug ? $actif : '' ?>
+                   rounded-lg px-5 py-2.5 text-center me-2 text-primary-blue hover:text-white border border-primary-blue hover:bg-primary-blue focus:ring-4 focus:outline-none focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-primary-blue"
+                   title="<?= $cat->category_description ?>">
+                    <?= $cat->name ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <?php
