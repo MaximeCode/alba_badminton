2bb545716f910bf864917e1f5d9c8e055ba350eb
Maxime Baude <108615340+MaximeCode@users.noreply.github.com>
2025-08-02 00:54:59 +0200

    TikTok link in contact page and join us page added

--- wp-content/themes/alba_theme/contact.php
+++ wp-content/themes/alba_theme/contact.php
@@ -60,7 +60,7 @@ $message = $_SESSION['contact_form']['message'] ?? '';
       <section class="w-full md:w-10/12 lg:max-xl:w-full xl:w-10/12 mx-auto bg-white px-5 py-3 rounded-2xl">
         <h4 class="text-2xl font-bold">Contactez-nous :</h4>
         <address
-          class="flex flex-col not-italic gap-y-8 md:gap-y-12 my-8 md:my-12 md:text-lg text-center md:text-left">
+          class="flex flex-col not-italic gap-y-6 md:gap-y-8 my-7 md:my-10 md:text-lg text-center md:text-left">
           <!--Email-->
           <p class="inline-flex items-center">
             <svg class="<?= $size_svg ?> text-primary-blue basis-1/12 md:basis-2/12" aria-hidden="true"
@@ -115,7 +115,20 @@ $message = $_SESSION['contact_form']['message'] ?? '';
             <span class="font-bold basis-5/12 md:basis-4/12">Sur Facebook :</span>
             <a href="https://www.facebook.com/profile.php?id=61561571563796&locale=fr_FR" target="_blank"
                class="text-lg md:text-xl italic basis-6/12 md:basis-6/12 hover:text-primary-blue hover:underline">
-              Amicale de Lucé Badminton
+              Amicale de Lucé Badminton - Officiel
+            </a>
+          </div>
+          <!--TikTok-->
+          <div class="inline-flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" fill="#1fa2da" class="<?= $size_svg ?> text-primary-blue basis-1/12 md:basis-2/12">
+              <!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+              <path
+                d="M544.5 273.9C500.5 274 457.5 260.3 421.7 234.7L421.7 413.4C421.7 446.5 411.6 478.8 392.7 506C373.8 533.2 347.1 554 316.1 565.6C285.1 577.2 251.3 579.1 219.2 570.9C187.1 562.7 158.3 545 136.5 520.1C114.7 495.2 101.2 464.1 97.5 431.2C93.8 398.3 100.4 365.1 116.1 336C131.8 306.9 156.1 283.3 185.7 268.3C215.3 253.3 248.6 247.8 281.4 252.3L281.4 342.2C266.4 337.5 250.3 337.6 235.4 342.6C220.5 347.6 207.5 357.2 198.4 369.9C189.3 382.6 184.4 398 184.5 413.8C184.6 429.6 189.7 444.8 199 457.5C208.3 470.2 221.4 479.6 236.4 484.4C251.4 489.2 267.5 489.2 282.4 484.3C297.3 479.4 310.4 469.9 319.6 457.2C328.8 444.5 333.8 429.1 333.8 413.4L333.8 64L421.8 64C421.7 71.4 422.4 78.9 423.7 86.2C426.8 102.5 433.1 118.1 442.4 131.9C451.7 145.7 463.7 157.5 477.6 166.5C497.5 179.6 520.8 186.6 544.6 186.6L544.6 274z" />
+            </svg>
+            <span class="font-bold basis-5/12 md:basis-4/12">Sur TikTok :</span>
+            <a href="https://www.tiktok.com/@alba.bad" target="_blank"
+               class="text-lg md:text-xl italic basis-6/12 md:basis-6/12 hover:text-primary-blue hover:underline">
+              @Alba.bad
             </a>
           </div>
         </address>
--- wp-content/themes/alba_theme/index.php
+++ wp-content/themes/alba_theme/index.php
@@ -1,5 +1,14 @@
 <?php get_header() ?>
 
-
-
+<?php
+if (have_posts()) {
+  while (have_posts()) {
+    the_post();
+    echo '<h2 class="text-4xl font-bold mb-12">' . get_the_title() . '</h2>';
+    echo "<div class='prose text-lg lg:text-xl'>";
+    the_content();
+    echo "</div>";
+  }
+}
+?>
 <?php get_footer() ?>
