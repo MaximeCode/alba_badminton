551d613cfd3b3829900ab3d3137bf10539cfa7e7
Maxime Baude <108615340+MaximeCode@users.noreply.github.com>
2025-10-12 23:43:36 +0200

    videur added, 404 updated & global articles and pages updated in tailwind.config.js

--- wp-content/themes/alba_theme/404.php
+++ wp-content/themes/alba_theme/404.php
@@ -2,19 +2,26 @@
 <section class="container text-center">
     <h2 class="text-4xl md:text-6xl font-bold text-primary-blue">
         Oops ! Page non trouvée.</h2>
-    <?= wp_get_attachment_image(521, '', false, array('class' => 'h-96 w-auto mx-auto hover:animate-spin')); ?>
+    <?= wp_get_attachment_image(
+        get_attachment_id_by_title('404_img'),
+        '',
+        false,
+        array('class' => 'h-96 w-auto mx-auto')
+    ); ?>
     <p class="text-gray-600 text-xl">
-        La page que vous recherchez semble introuvable. <br>
-        Vous pouvez revenir à la page d'accueil pour explorer davantage.<br>
-        Ou vous pouvez rechercher une ressource grâce au formulaire ci-dessous.
+        <strong>La page que vous recherchez semble introuvable.</strong><br>
+        Vous pouvez rechercher une ressource présente sur le site grâce au formulaire ci-dessous.
     </p>
-
-    <div class="max-w-md mx-auto mt-8">
+    <div class="max-w-md mx-auto my-4">
         <?= get_search_form() ?>
     </div>
 
+    <p class="text-gray-600 text-xl">
+        Vous pouvez également revenir à la page d'accueil pour explorer davantage.
+    </p>
+
     <a href="/"
-       class="text-xl mt-6 inline-block px-8 py-3 text-white bg-primary-blue rounded-lg shadow-lg hover:bg-secondary-blue transition-all">
+        class="text-xl mt-6 inline-block px-8 py-3 text-white bg-primary-blue rounded-lg shadow-lg hover:bg-secondary-blue transition-all">
         Retour à l'accueil
     </a>
 </section>
